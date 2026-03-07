01719d94d7cd66cb8b783c05cef9409654a24990
Oscar Alberto ROMAN <roman292@gmail.com>
2026-03-07 13:25:32 -0300

    fix: sitio estatico simple

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
-import Nav from '@/components/Nav'
-import Hero from '@/components/Hero'
-import Metrics from '@/components/Metrics'
-import Problema from '@/components/Problema'
-import Soluciones from '@/components/Soluciones'
-import Proceso from '@/components/Proceso'
-import CtaFinal from '@/components/CtaFinal'
-import Footer from '@/components/Footer'
-import ChatbotPlaceholder from '@/components/ChatbotPlaceholder'
-
 export default function Home() {
   return (
-    <main>
-      <Nav />
-      <Hero />
-      <Metrics />
-      <Problema />
-      <Soluciones />
-      <Proceso />
-      <CtaFinal />
-      <Footer />
-      <ChatbotPlaceholder />
-    </main>
+    <script dangerouslySetInnerHTML={{__html: `window.location.href='/index.html'`}} />
   )
 }
